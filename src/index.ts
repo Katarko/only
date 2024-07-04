04984a4c5c714df84feb82df245212fe52824bbb
Nick Mack <makoldinn@mail.ru>
2024-07-04 17:52:39 +0700

    Upd

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as _ from "lodash";
 import "./scss/style.scss";
-import { register } from "swiper/element/bundle";
-// register Swiper custom elements
-register(); //import Icon from "./img/icon.png";
+import Swiper from "swiper";
+// import Swiper styles
+import "swiper/css";
+
 let events = [
     { id: 0, parentId: 1, title: "2000", description: "" },
     { id: 1, parentId: 1, title: "2000", description: "" },
@@ -13,25 +14,12 @@ let events = [
 ];
 
 function component() {
-    const section = document.createElement("div");
-    const title = document.createElement("h2");
-    const swiperContainer = document.createElement("div");
-    const swiperSlide = document.createElement("div");
-
-    section.classList.add("section");
-    section.appendChild(title);
-    section.appendChild(swiperContainer);
-    title.innerHTML = "Исторические даты";
-    title.classList.add("section__title");
-    swiperContainer.classList.add("swiper-container");
-    swiperSlide.classList.add("swiper-slide");
-    swiperSlide.innerHTML = "1";
-    events.forEach(function (element) {
-        let event = new Event(element.id, element.parentId, element.title, element.description);
-    });
-
-    return section;
+    return;
 }
+events.forEach(function (element) {
+    let event = new Event(element.id, element.parentId, element.title, element.description);
+});
+
 class Event {
     id: number;
     parentId: number;
@@ -44,14 +32,12 @@ class Event {
         this.description = eventDescription;
     }
 }
-// new Swiper(".swiper", {
-//     slidesPerView: 3,
-//     grid: {
-//         rows: 3,
-//     },
-//     mousewheel: {
-//         forceToAxis: true,
-//     },
-// });
-
-document.body.appendChild(component());
+new Swiper(".swiper", {
+    slidesPerView: 3,
+    grid: {
+        rows: 3,
+    },
+    mousewheel: {
+        forceToAxis: true,
+    },
+});
