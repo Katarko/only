2af0e0c40502fc9c6499c5254692bae0317faeee
Nick Mack <makoldinn@mail.ru>
2024-07-08 16:28:01 +0700

    Circle nav upd

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ function component() {
 // events.forEach(function (element) {
 //     let event = new Event(element.id, element.parentId, element.title, element.description);
 // });
-
+const titles: string[] = ["Заголовок", "Кино", "Литература", "Театр", "Заголовок", "Наука"];
+document.querySelector<HTMLElement>(".period-slider__pagination-circle").style.transform = "rotate(0deg)";
 class Event {
     id: number;
     parentId: number;
@@ -37,7 +38,7 @@ class Event {
         this.description = eventDescription;
     }
 }
-let mainSwiper = new Swiper(".time-period-slider__swiper", {
+let mainSwiper = new Swiper(".period-slider__swiper", {
     modules: [Navigation, Pagination, EffectFade, Controller],
     slidesPerView: 1,
     effect: "fade",
@@ -46,19 +47,41 @@ let mainSwiper = new Swiper(".time-period-slider__swiper", {
         crossFade: true,
     },
     pagination: {
-        el: ".time-period-slider__pagination",
+        el: ".period-slider__pagination",
         type: "fraction",
     },
     navigation: {
-        prevEl: ".time-period-slider__btn_prev",
-        nextEl: ".time-period-slider__btn_next",
+        prevEl: ".period-slider__btn_prev",
+        nextEl: ".period-slider__btn_next",
+    },
+    on: {
+        slideChangeTransitionStart: function () {
+            gsap.effects.fadeOut(".swiper-pagination-bullet__title");
+        },
+        slideChangeTransitionEnd: function () {
+            gsap.effects.fadeIn(".swiper-pagination-bullet-active .swiper-pagination-bullet__title");
+        },
     },
 });
-let pagingSwiper = new Swiper(".time-period-slider__swiper", {
+let pagingSwiper = new Swiper(".period-slider__swiper", {
     modules: [Pagination, Controller],
     pagination: {
-        el: ".time-period-slider__pagination-circle",
+        el: ".period-slider__pagination-circle",
         clickable: true,
+    },
+    on: {
+        afterInit: function () {
+            alignBullets();
+        },
+        slideChange: function () {
+            rotateBullets();
+        },
+        slideChangeTransitionStart: function () {
+            gsap.effects.fadeOut(".swiper-pagination-bullet__title");
+        },
+        slideChangeTransitionEnd: function () {
+            gsap.effects.fadeIn(".swiper-pagination-bullet-active .swiper-pagination-bullet__title");
+        },
     },
 });
 
@@ -78,25 +101,58 @@ let eventSwiper = new Swiper(".event-slider__swiper", {
         nextEl: ".event-slider__btn_next",
     },
 });
-
-function animate() {
-    // const items = document.querySelectorAll(".time-period-item__val");
-    // let a = [500, 1500];
-    // console.log(items);
-    // gsap.from(items, {
-    //     textContent: a,
-    //     duration: 4,
-    //     ease: "power1.in",
-    //     snap: { textContent: 1 },
-    // });
-    // gsap.utils.toArray(".time-period-item__val").forEach((box) => {
-    //     var spin = box.getAttribute("data-spin");
-    //     let tween = gsap.from(box, {
-    //         textContent: spin,
-    //         duration: 4,
-    //         ease: "power1.in",
-    //         snap: { textContent: 1 },
-    //     });
-    // });
+function alignBullets() {
+    let bullets = document.querySelectorAll<HTMLElement>(".swiper-pagination-bullet");
+    let container = document.querySelector<HTMLElement>(".period-slider__pagination-circle");
+    let diameter = container.offsetWidth,
+        angle = -45,
+        radius = diameter / 2,
+        step = (2 * Math.PI) / bullets.length,
+        bulletWidth = bullets[0].offsetWidth;
+    for (let i = 0; i < bullets.length; i++) {
+        let x = Math.round(diameter / 2 + radius * Math.cos(angle) - bulletWidth / 2);
+        let y = Math.round(diameter / 2 + radius * Math.sin(angle) - bulletWidth / 2);
+        bullets[i].style.left = x + "px";
+        bullets[i].style.top = y + "px";
+        let bulletTitle = document.createElement("div");
+        let bulletIndex = document.createElement("div");
+        bulletTitle.className = "swiper-pagination-bullet__title";
+        bulletIndex.className = "swiper-pagination-bullet__index";
+        bulletTitle.textContent = titles[i];
+        bulletIndex.textContent = (i + 1).toString();
+        bullets[i].appendChild(bulletTitle);
+        bullets[i].appendChild(bulletIndex);
+        angle += step;
+    }
 }
-animate();
+function rotateBullets() {
+    let bullets = document.querySelectorAll<HTMLElement>(".swiper-pagination-bullet");
+    let container = document.querySelector<HTMLElement>(".period-slider__pagination-circle");
+    let rotateAngle = ((pagingSwiper.activeIndex - pagingSwiper.previousIndex) * 360) / bullets.length;
+    if (rotateAngle >= 180) {
+        rotateAngle = ((rotateAngle - 360) * -1) % 360;
+    } else if (rotateAngle < -180) {
+        rotateAngle = ((rotateAngle + 360) * -1) % 360;
+    } else {
+        rotateAngle = -rotateAngle;
+    }
+    let newAngle = parseInt(container.style.getPropertyValue("transform").replace(/(?!-)[^0-9.]/g, "")) + rotateAngle;
+    container.style.transform = "rotate(" + newAngle + "deg)";
+    for (let i = 0; i < bullets.length; i++) {
+        bullets[i].style.transform = "rotate(" + -newAngle + "deg)";
+    }
+}
+gsap.registerEffect({
+    name: "fadeIn",
+    effect: (targets: HtmlTagObject, config: any) => {
+        return gsap.to(targets, { duration: config.duration, opacity: 1 });
+    },
+    defaults: { duration: 0.4 }, //defaults get applied to any "config" object passed to the effect
+});
+gsap.registerEffect({
+    name: "fadeOut",
+    effect: (targets: HtmlTagObject, config: any) => {
+        return gsap.to(targets, { duration: config.duration, opacity: 0 });
+    },
+    defaults: { duration: 0.1 }, //defaults get applied to any "config" object passed to the effect
+});
